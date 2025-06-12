c21aa066e751e8e37bd0df9735a9285dd6a6a067
Thonghuy10 <thongnhh2019096@gmail.com>
2025-06-12 21:21:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import base64
 from datetime import date
-from .pages.homepages import show_homepages
+from pages.homepages import show_homepages
 from pages.overview import show_overview
 from pages.visualizations import show_visualizations
 from pages.insights import show_insights
